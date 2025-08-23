5534a9ef83bec9f76282ee9c3bed2723c0bf24bf
tiagozheng <tiago.zheng23@gmail.com>
2025-08-23 17:51:07 +0100

    code clean up

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, deleteProduct, getProducts, updatedProduct } from "../controller/product.controller";
+import { createProduct, deleteProduct, getProducts, updatedProduct } from "../controller/product.controller.js";
 
 const router = express.Router();
 
