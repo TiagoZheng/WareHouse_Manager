423f463c6c37818507236c11380b80ccd6331534
tiagozheng <tiago.zheng23@gmail.com>
2025-08-23 12:02:56 +0100

    create Post Request

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,24 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.barcode) {
+    return res.status(400).json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    conseole.error(`Error in Creating product: ${error.message}`);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 app.listen(5000, () => {
   connectDB();
   console.log("Server started");
