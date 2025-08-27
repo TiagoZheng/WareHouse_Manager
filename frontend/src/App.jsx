fd4ea045d65ea8db823dbe1370fd336cddcdb445
tiagozheng <tiago.zheng23@gmail.com>
2025-08-27 11:33:55 +0100

    update Inventory component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,13 @@ import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-amber-400">
       <SideBarMenu />
 
       <div className="flex-1 flex-col flex">
         <TopBar />
 
-        <div className="flex-1 flex bg-amber-800 align-middle justify-center">
+        <div className="flex-1 flex bg-gray-100">
           <Inventory />
         </div>
       </div>
--- frontend/src/components/Inventory.jsx
+++ frontend/src/components/Inventory.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Inventory = () => {
   return (
-    <div className="flex flex-col flex-1 m-8 bg-blue-300">
-      <div className="flex flex-[1] bg-cyan-800 font-bold text-4xl">Inventory</div>
-      <div className="flex flex-[9] bg-cyan-50">Table</div>
+    <div className="flex flex-col flex-1 m-8 ">
+      <div className="mb-8">
+        <p className=" text-2xl font-bold">Inventory</p>
+        <p>Dashboard / Inventory</p>
+      </div>
+      <div className="flex flex-[9] bg-cyan-50 rounded-2xl p-8">Table</div>
     </div>
   );
 };
